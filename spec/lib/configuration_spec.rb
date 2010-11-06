c332db4dd94c5977cc115f79043258e2dfa0c58d
adriandulic <adulic@gmail.com>
2010-11-06 14:33:51 +0100

    spec for Configuration.format

--- spec/lib/configuration_spec.rb
+++ spec/lib/configuration_spec.rb
@@ -4,7 +4,9 @@ describe "Configuration" do
   it "should store configuration information in class attributes" do
     BitsOnTheRun::Configuration.key    = "Ax4kflJt"
     BitsOnTheRun::Configuration.secret = "nUwCMz1UaciitFj6uEEpoXHn"
-    BitsOnTheRun::Configuration.key.should    eql("Ax4kflJt")
-    BitsOnTheRun::Configuration.secret.should eql("nUwCMz1UaciitFj6uEEpoXHn")
+
+    BitsOnTheRun::Configuration.key.should    == "Ax4kflJt"
+    BitsOnTheRun::Configuration.secret.should == "nUwCMz1UaciitFj6uEEpoXHn"
+    BitsOnTheRun::Configuration.format.should == "json"
   end
 end
